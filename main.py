09ab4d733de3dbc246f610005df0e8c59bcbcbbb
hugo-bec <hugo.bec@etu.umontpellier.fr>
2023-02-21 23:54:09 +0100

    sort message and save song names in files

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from twitchio.ext import commands
 from twitchio.client import Client
 from dotenv import load_dotenv
+from datetime import date, datetime
 import os
 
 load_dotenv()
@@ -24,23 +25,18 @@ client = Client(
 @bot.event
 async def event_message(ctx):
     if ctx.author.name == "waifu4u":
-        print("BOT: ", ctx.content)
-    else:
-        print(ctx.author.name)
-    #print(ctx.content)
-    #await bot.handle_commands(ctx)
-
-
-'''@bot.command(name='test')
-async def test_command(ctx):
-    await ctx.send("this is a test response")
-
-
-@bot.command(name='who')
-async def get_chatters(ctx):
-    chatters = await client.get_chatters('incompetent_ian')
-    all_chatters = ' '.join(chatters.all)
-    await ctx.send(f"In chat: {all_chatters}")'''
+        message = ctx.content
+        if message[:7] == "wtfSalt":
+            today_str = date.today().strftime("%Y-%m_%d")
+            now_str = datetime.now().strftime("%Y-%m-%d_%H:%M")
+            song_history = now_str + " " + message[10:]
+            path_folder = "/var/www/html/twitch-bot/history-saltysongs/"
+            f = open(path_folder + "saltysongs_" + today_str + ".txt", "a")
+            f.write(song_history + "\n")
+            f.close()
+            #print("song saved !")
+        #if ctx.content
+        #print("BOT: ", ctx.content)
 
 
 if __name__ == '__main__':
